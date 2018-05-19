25153a1fb47d3fb27cfd40b6b1fa5410cd5808e3
dhaval27594 <36393384+dhaval27594@users.noreply.github.com>
2018-05-19 21:21:13 +0530

    Update adminController.php

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -146,7 +146,7 @@ class adminController extends Controller
 
 	public function jobpostlist()
     {
-        $joblist=jobPostModel::get();
+        $joblist=jobPostModel::get(); dd($joblist);
         return view('admin.company.jobPostList',['joblist'=>$joblist]);
     }
 
